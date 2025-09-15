1c6054477e91eaabb89450ee879a72ca6bf0ea33
Neris B.D.F.E. <it24100782@my.sliit.lk>
2025-09-15 20:56:58 +0530

    IT24100782 Lab 6 Submitted

---
+++ Lab Sheet 06/IT24100782_PS_Lab_6.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\ADMIN\\Desktop\\IT24100782")
+
+#Exercise
+#Part 1
+
+X~ Binomial(n=50,p=0.85)
+1- pbinom(46,size = 50,prob=0.85)
+
+
+#Part 2
+#Define the random variable
+#X =number of calls in one hour
+#Distribution
+#X~ Poisson(lambda =12)
+dpois(15,lambda =12)
